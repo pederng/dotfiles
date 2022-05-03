fb4ce648fa4b87675bde4ebeaccf369f41540918
Peder Notto Galteland <peder.galteland@softwarelab.no>
2022-05-03 14:57:51 +0200

    add todo-comments.nvim, fix async autoformatting function

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -68,6 +68,10 @@ require('packer').startup(function(use)
 
   -- Visuals
   use { 'folke/trouble.nvim', branch = 'main' }
+  use { "folke/todo-comments.nvim", config = function()
+    require("todo-comments").setup {}
+  end
+  }
   use { 'romgrk/nvim-treesitter-context' }
   use { 'nvim-lualine/lualine.nvim' }
   use { 'akinsho/bufferline.nvim', tag = '*' }
@@ -245,19 +249,49 @@ local sources = {
   null_ls.builtins.code_actions.gitsigns,
 }
 
+local async_formatting = function(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+
+  vim.lsp.buf_request(
+    bufnr,
+    "textDocument/formatting",
+    { textDocument = { uri = vim.uri_from_bufnr(bufnr) } },
+    function(err, res)
+    if err then
+      local err_msg = type(err) == "string" and err or err.message
+      vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+      return
+    end
+
+    if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+      return
+    end
+
+    if res then
+      vim.lsp.util.apply_text_edits(res, bufnr, 'utf-8')
+      vim.api.nvim_buf_call(bufnr, function()
+        vim.cmd("silent noautocmd update")
+      end)
+    end
+  end
+  )
+end
+
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
-    sources = sources,
-		on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-						vim.api.nvim_create_autocmd("BufWritePre", {
-								group = augroup,
-								buffer = bufnr,
-								callback = function() vim.lsp.buf.format{async=true} end
-						})
-				end
-		end,
+  sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          async_formatting(bufnr)
+        end
+      })
+    end
+  end,
 })
 
 require('gitsigns').setup {
